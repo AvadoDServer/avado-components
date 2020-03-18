c4b47554ed560269c0e6d3f64d6e391e9bef8734
Stefaan Ponnet <stefaan@ponnet.com>
2020-03-18 17:04:55 +0100

    cleaned up HTML

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -14,56 +14,35 @@ const Navigation = ({
                         <Icons.Logo fill="#000" />
                     </span>
                 </a>
-
                 <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
             </div>
-
             <div id="navbarBasicExample" className="navbar-menu">
                 <div className="navbar-start">
-                    <a className="navbar-item">
-                        Home
-        </a>
-
-                    <a className="navbar-item">
-                        Documentation
-        </a>
-
+                    <a className="navbar-item">Blijf</a>
+                    <a className="navbar-item">In</a>
+                    <a className="navbar-item">Uw</a>
                     <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">
-                            More
-          </a>
-
+                        <a className="navbar-link">Kot</a>
                         <div className="navbar-dropdown">
-                            <a className="navbar-item">
-                                About
-            </a>
-                            <a className="navbar-item">
-                                Jobs
-            </a>
-                            <a className="navbar-item">
-                                Contact
-            </a>
+                            <a className="navbar-item">Zegt</a>
+                            <a className="navbar-item">Maggie</a>
+                            <a className="navbar-item">De</a>
                             <hr className="navbar-divider" />
-                            <a className="navbar-item">
-                                Report an issue
-            </a>
+                            <a className="navbar-item">Block</a>
                         </div>
                     </div>
                 </div>
-
                 <div className="navbar-end">
                     <div className="navbar-item">
                         <div className="buttons">
                             <a className="button is-primary">
                                 <strong>Sign up</strong>
                             </a>
-                            <a className="button is-light">
-                                Log in
-            </a>
+                            <a className="button is-light">Log in</a>
                         </div>
                     </div>
                 </div>
